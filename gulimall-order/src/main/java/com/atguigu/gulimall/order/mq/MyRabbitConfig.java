26a753c43db0e0d31c9472d195bab5bde10f36a7
coderZzc <669296585@qq.com>
2022-07-17 19:04:56 +0800

    删除无用代码

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/mq/MyRabbitConfig.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/mq/MyRabbitConfig.java
@@ -1,22 +1,17 @@
 package com.atguigu.gulimall.order.mq;
 
-import com.atguigu.gulimall.order.exception.MqConfirmCallbackException;
-import com.atguigu.gulimall.order.exception.MqReturnCallbackException;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,11 +30,6 @@ public class MyRabbitConfig {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         myRabbitTemplate = rabbitTemplate;
         myRabbitTemplate.setMessageConverter(messageConverter());
-        //设置确认回调
-//        myRabbitTemplate.setConfirmCallback((correlationData,ack,cause) -> {
-//            System.out.println("confirm...correlationData["+correlationData+"]==>ack:["+ack+"]==>cause:["+cause+"]");
-//        });
-        initRabbitTemplate();
         return myRabbitTemplate;
     }
 
@@ -47,53 +37,6 @@ public class MyRabbitConfig {
     public MessageConverter messageConverter(){
         return new Jackson2JsonMessageConverter();
     }
-
-    /**
-     * 定制RabbitTemplate
-     * 1、服务收到消息就会回调
-     *      1、spring.rabbitmq.publisher-confirms: true
-     *      2、设置确认回调
-     * 2、消息正确抵达队列就会进行回调
-     *      1、spring.rabbitmq.publisher-returns: true
-     *         spring.rabbitmq.template.mandatory: true
-     *      2、设置确认回调ReturnCallback
-     *
-     * 3、消费端确认(保证每个消息都被正确消费，此时才可以broker删除这个消息)
-     *
-     */
-    // @PostConstruct  //MyRabbitConfig对象创建完成以后，执行这个方法
-    public void initRabbitTemplate() {
-        /**
-         * 1、只要消息抵达Broker就ack=true
-         * correlationData：当前消息的唯一关联数据(这个是消息的唯一id)
-         * ack：消息是否成功收到
-         * cause：失败的原因
-         */
-        //设置确认回调
-        myRabbitTemplate.setConfirmCallback((correlationData,ack,cause) -> {
-//            System.out.println("confirm...correlationData["+correlationData+"]==>ack:["+ack+"]==>cause:["+cause+"]");
-            if(!ack){
-                throw new MqConfirmCallbackException("confirm...correlationData["+correlationData+"]==>ack:["+ack+"]==>cause:["+cause+"]");
-            }else {
-                System.out.println("confirm...correlationData["+correlationData+"]==>ack:["+ack+"]==>cause:["+cause+"]");
-            }
-        });
-
-        /**
-         * 只要消息没有投递给指定的队列，就触发这个失败回调
-         * message：投递失败的消息详细信息
-         * replyCode：回复的状态码
-         * replyText：回复的文本内容
-         * exchange：当时这个消息发给哪个交换机
-         * routingKey：当时这个消息用哪个路邮键
-         */
-        myRabbitTemplate.setReturnCallback((message,replyCode,replyText,exchange,routingKey) -> {
-            String msg = "Fail Message["+message+"]==>replyCode["+replyCode+"]" +
-                    "==>replyText["+replyText+"]==>exchange["+exchange+"]==>routingKey["+routingKey+"]";
-            throw new MqReturnCallbackException(msg);
-        });
-    }
-
 
     @Bean
     public Exchange orderEventExchange(){
@@ -123,10 +66,6 @@ public class MyRabbitConfig {
                 ,null);
     }
 
-//    @RabbitListener(queues = {"order.release.order.queue"})
-//    public void initQueueAndExchange(){
-//
-//    }
     @Bean
     public Binding orderDelayBinding(){
         return new Binding("order.delay.queue"
